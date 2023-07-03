48b1d11c2fb430b14646435318e9fb2f694d2177
gazizovdastan <gazizovdastan@gmail.com>
2023-07-03 11:49:17 +0500

    создал UI элементы

--- hw12/App/Sources/ViewController.swift
+++ hw12/App/Sources/ViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 import SnapKit
 
 class ViewController: UIViewController {
+    
+// MARK: - State
+    var isStarted = false
+    var isRunning = false
+    var isWorkTime = true
+    var ccunter = 25
+    var remaininTime = 0
+    var timer: Timer = Timer()
 
 // MARK: - Lifcycle
 
@@ -19,7 +27,7 @@ class ViewController: UIViewController {
 
     }
     
-// MARK: - Outlets
+// MARK: - UI
     
     private lazy var backgroundImageView: UIImageView = {
         let imageView = UIImageView()
@@ -29,13 +37,25 @@ class ViewController: UIViewController {
     
     private var timerLabel: UILabel = {
         let label = UILabel()
-        
+        label.text = "00:00"
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 50)
         return label
     }()
     
+    private var progressBar: CircularProgressView = {
+        let progressView = CircularProgressView(frame: CGRect(x: -90, y: -35, width: 180, height: 180), lineWidth: 5, rounded: false)
+        progressView.trackTintColor = .white
+        progressView.progressTintColor = .red
+        return progressView
+    }()
+    
     private var playPauseButton: UIButton = {
-        let button = UIButton()
-        
+        let button = UIButton(type: .system)
+        button.setImage("", for: .normal)
+        button.tintColor = .white
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpOutside)
+        button.setPreferredSymbolConfiguration(UIImage.SymbolConfiguration(pointSize: 50), forImageIn: .normal)
         return button
     }()
 
@@ -43,6 +63,10 @@ class ViewController: UIViewController {
     
     private func setupHierarchy() {
         view.addSubview(backgroundImageView)
+        view.addSubview(timerLabel)
+        view.addSubview(progressBar)
+        view.addSubview(playPauseButton)
+        
     }
     
     private func setupViews() {
